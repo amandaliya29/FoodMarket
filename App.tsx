c4e9cdf15c4df82903227a6b24163d688fddda41
Ashish <ajmandaliya290@gmail.com>
2024-09-28 22:24:04 +0530

    add all screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SplashScreen from './src/components/SpleshScreen';
 import HomeScreen from './src/components/HomeScreen'; // Create this screen
 
 const Stack = createNativeStackNavigator();
@@ -9,12 +8,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-          options={{headerShown: false}}
-        />
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
